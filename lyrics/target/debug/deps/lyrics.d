c747cfa6e98cbf9dfc72bfb2c2522a768f0bc998
adarsh-v-h <adarshvh2005@gmail.com>
2025-09-01 17:23:04 +0530

    changes

--- lyrics/target/debug/deps/lyrics.d
+++
@@ -1,5 +0,0 @@
-C:\Users\adars\OneDrive\Documents\vs code\RUST\Practice questions\lyrics\target\debug\deps\lyrics.d: src\main.rs
-
-C:\Users\adars\OneDrive\Documents\vs code\RUST\Practice questions\lyrics\target\debug\deps\lyrics.exe: src\main.rs
-
-src\main.rs:
--- lyrics/target/debug/lyrics.d
+++
@@ -1,1 +0,0 @@
-C:\Users\adars\OneDrive\Documents\vs\ code\RUST\Practice\ questions\lyrics\target\debug\lyrics.exe: C:\Users\adars\OneDrive\Documents\vs\ code\RUST\Practice\ questions\lyrics\src\main.rs
