7258d0ef1b789aa3c60c307035b939e0d22bfe61
adarsh-v-h <adarshvh2005@gmail.com>
2025-09-02 16:36:52 +0530

    removing certain files

---
+++ lyrics/target/debug/deps/lyrics.d
@@ -0,0 +1,5 @@
+C:\Users\adars\OneDrive\Documents\vs code\RUST\Practice questions\lyrics\target\debug\deps\lyrics.d: src\main.rs
+
+C:\Users\adars\OneDrive\Documents\vs code\RUST\Practice questions\lyrics\target\debug\deps\lyrics.exe: src\main.rs
+
+src\main.rs:
---
+++ lyrics/target/debug/lyrics.d
@@ -0,0 +1,1 @@
+C:\Users\adars\OneDrive\Documents\vs\ code\RUST\Practice\ questions\lyrics\target\debug\lyrics.exe: C:\Users\adars\OneDrive\Documents\vs\ code\RUST\Practice\ questions\lyrics\src\main.rs
